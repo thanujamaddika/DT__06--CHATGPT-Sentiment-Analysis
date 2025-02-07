ee6b1657813b606f17992285823b4e415340cb23
thanujamaddika <thanujamaddika2357@gmail.com>
2025-02-07 21:58:31 +0530

    ..

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask,render_template
-app=Flask(__name__)
+from flask import Flask, render_template, request, jsonify
+
+app = Flask(__name__)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
+@app.route("/predict", methods=["GET", "POST"])
+def predict():
+    if request.method == "POST":
+        msg = request.form.get("message")
+        print(msg)  # Debugging
+        return jsonify({"message": msg})  # Return a valid response
+    return render_template("predict.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
